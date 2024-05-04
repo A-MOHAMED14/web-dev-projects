09abec1716794c3e5f6ad3c1cf2b283e9c3ac619
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-05-04 16:41:30 +0100

    Establish PostgreSQL connection and query database for data

--- Backend/8.3+Travel+Tracker/8.3 Travel Tracker/index.js
+++ Backend/8.3+Travel+Tracker/8.3 Travel Tracker/index.js
@@ -1,11 +1,37 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "code4funloop4ever!",
+  port: 5432,
+});
 
 const app = express();
 const port = 3000;
 
+db.connect;
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+let countriesVisited;
+let totalCountries;
+
+db.query("SELECT country_code FROM visited_countries", (err, res) => {
+  if (err) {
+    console.error("Error querying database:", err);
+  } else {
+    countriesVisited = res.rows;
+    console.log(countriesVisited, "<-------");
+    totalCountries = countriesVisited.length;
+    console.log(totalCountries, "*********");
+  }
+  db.end();
+});
 
 app.get("/", async (req, res) => {
   //Write your code here.
