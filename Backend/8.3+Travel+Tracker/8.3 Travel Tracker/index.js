51462425a0dbf834eef9000face1d41c80425660
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-05-04 17:21:50 +0100

    Refactor GET route handler

--- Backend/8.3+Travel+Tracker/8.3 Travel Tracker/index.js
+++ Backend/8.3+Travel+Tracker/8.3 Travel Tracker/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 
+const app = express();
+const port = 3000;
+
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
@@ -10,31 +13,36 @@ const db = new pg.Client({
   port: 5432,
 });
 
-const app = express();
-const port = 3000;
-
 db.connect;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let countriesVisited;
-let totalCountries;
-
-db.query("SELECT country_code FROM visited_countries", (err, res) => {
-  if (err) {
-    console.error("Error querying database:", err);
-  } else {
-    countriesVisited = res.rows;
-    console.log(countriesVisited, "<-------");
-    totalCountries = countriesVisited.length;
-    console.log(totalCountries, "*********");
-  }
-  db.end();
-});
-
 app.get("/", async (req, res) => {
-  res.render("index.ejs", { countries: countriesVisited, total: totalCountries });
+  try {
+    const response = await db.query(
+      "SELECT country_code FROM visited_countries"
+    );
+
+    let countriedVisited = [];
+
+    response.rows.forEach((country) => {
+      countriedVisited.push(country.country_code);
+    });
+
+    console.log(response.rows, "<--------");
+    console.log(countriedVisited, "*******");
+
+    res.render("index.ejs", {
+      countries: countriedVisited,
+      total: countriedVisited.length,
+    });
+
+    db.end();
+  } catch (err) {
+    console.error("Error:", err);
+    // res.status(500).send("Internal Server Error");
+  }
 });
 
 app.listen(port, () => {
