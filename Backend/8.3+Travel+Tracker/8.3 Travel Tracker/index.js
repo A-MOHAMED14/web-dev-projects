c795d618fe02ef3b3d5398c368e2217a33199c7d
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-05-05 13:26:25 +0100

    Refactor GET and POST routes

--- Backend/8.3+Travel+Tracker/8.3 Travel Tracker/index.js
+++ Backend/8.3+Travel+Tracker/8.3 Travel Tracker/index.js
@@ -18,62 +18,52 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+async function checkVisistedCountries() {
+  const response = await db.query("SELECT country_code FROM visited_countries");
+
+  let countriesVisited = [];
+
+  response.rows.forEach((country) => {
+    countriesVisited.push(country.country_code);
+  });
+
+  return countriesVisited;
+}
+
 app.get("/", async (req, res) => {
   try {
-    const response = await db.query(
-      "SELECT country_code FROM visited_countries"
-    );
-
-    let countriesVisited = [];
-
-    response.rows.forEach((country) => {
-      countriesVisited.push(country.country_code);
-    });
-
-    console.log(response.rows, "<--------");
-    console.log(countriesVisited, "*******");
+    const countriesVisited = await checkVisistedCountries();
 
     res.render("index.ejs", {
       countries: countriesVisited,
       total: countriesVisited.length,
     });
-
-    // db.end();
   } catch (err) {
     console.error("Error:", err);
   }
 });
 
 app.post("/add", async (req, res) => {
-  // console.log(req.body, "<<<<<<<<");
-  const newCountryVisited = req.body.country;
+  try {
+    // console.log(req.body, "<<<<<<<<");
+    const newCountryVisited = req.body.country;
 
-  const response = await db.query(
-    "SELECT country_code FROM countries WHERE country_name = $1",
-    [newCountryVisited]
-  );
-  // console.log(response.rows[0], "$$$$$$$$$$");
+    const response = await db.query(
+      "SELECT country_code FROM countries WHERE country_name = $1",
+      [newCountryVisited]
+    );
+    // console.log(response.rows[0], "$$$$$$$$$$");
 
-  const newCountryCode = response.rows[0].country_code;
-  console.log(newCountryCode, "*********");
+    const newCountryCode = response.rows[0].country_code;
+    // console.log(newCountryCode, "*********");
 
-  await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
-    newCountryCode,
-  ]);
-
-  const result = await db.query("SELECT country_code FROM visited_countries");
-
-  let countriesVisited = [];
-
-  result.rows.forEach((country) => {
-    countriesVisited.push(country.country_code);
-  });
-
-  res.render("index.ejs", {
-    countries: countriesVisited,
-    total: countriesVisited.length,
-  });
-  db.end();
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      newCountryCode,
+    ]);
+    res.redirect("/");
+  } catch (err) {
+    console.error("Error querying database:", err);
+  }
 });
 
 app.listen(port, () => {
