473d1615e3f2f2f7f2248e91e038e64998588314
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-05-05 12:59:36 +0100

    Add POST route handler to insert new country into visited_countries table

--- Backend/8.3+Travel+Tracker/8.3 Travel Tracker/index.js
+++ Backend/8.3+Travel+Tracker/8.3 Travel Tracker/index.js
@@ -38,10 +38,42 @@ app.get("/", async (req, res) => {
       total: countriesVisited.length,
     });
 
-    db.end();
+    // db.end();
   } catch (err) {
     console.error("Error:", err);
   }
+});
+
+app.post("/add", async (req, res) => {
+  // console.log(req.body, "<<<<<<<<");
+  const newCountryVisited = req.body.country;
+
+  const response = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [newCountryVisited]
+  );
+  // console.log(response.rows[0], "$$$$$$$$$$");
+
+  const newCountryCode = response.rows[0].country_code;
+  console.log(newCountryCode, "*********");
+
+  await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+    newCountryCode,
+  ]);
+
+  const result = await db.query("SELECT country_code FROM visited_countries");
+
+  let countriesVisited = [];
+
+  result.rows.forEach((country) => {
+    countriesVisited.push(country.country_code);
+  });
+
+  res.render("index.ejs", {
+    countries: countriesVisited,
+    total: countriesVisited.length,
+  });
+  db.end();
 });
 
 app.listen(port, () => {
