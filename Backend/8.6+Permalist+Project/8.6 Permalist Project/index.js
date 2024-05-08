ecc3e9a28c69d13c0fcb7b3a6bf5f52fe8c97b69
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-05-08 16:02:04 +0100

    Add /delete endpoint for Post route

--- Backend/8.6+Permalist+Project/8.6 Permalist Project/index.js
+++ Backend/8.6+Permalist+Project/8.6 Permalist Project/index.js
@@ -56,17 +56,25 @@ app.post("/add", async (req, res) => {
 app.post("/edit", async (req, res) => {
   console.log(req.body, "<<<<<<<<<<");
   try {
-    const response = await db.query(
-      "UPDATE items SET title = $1 WHERE id = $2",
-      [req.body.updatedItemTitle, req.body.updatedItemId]
-    );
+    await db.query("UPDATE items SET title = $1 WHERE id = $2", [
+      req.body.updatedItemTitle,
+      req.body.updatedItemId,
+    ]);
     res.redirect("/");
   } catch (err) {
     console.error(err);
   }
 });
 
-app.post("/delete", (req, res) => {});
+app.post("/delete", async (req, res) => {
+  console.log(req.body, "<------");
+  try {
+    await db.query("DELETE FROM items WHERE id = $1", [req.body.deleteItemId]);
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
